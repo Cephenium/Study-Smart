294c1ecf64b7fe3acab10be4fc4bfeef03e4aaa3
Sunny ☀️ <sanay.nair@gmail.com>
2025-05-22 21:25:56 +0400

    installed react

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Card from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center lg:mx-70 md: mx-40 sm:mx-20 mx-10">
+        HELLO WORLD! i added a card component :D;
+    </div>
+  );
+}
